2cecb7db5fea8f797054cd8285c164affd43682d
raiden00pl <raiden00@railab.me>
2023-02-25 14:17:34 +0100

    sync with nxscli plugin loader

--- tests/test_nxsclinp.py
+++ tests/test_nxsclinp.py
@@ -1,9 +1,10 @@
 import nxscli_np
-import nxscli_np.plugin
+import nxscli_np.ext_plugins
+import nxscli_np.ext_commands
 
 
 def test_nxsclinp():
     assert nxscli_np.__version__
 
-    assert isinstance(nxscli_np.plugin.plugins_list, list)
-    assert isinstance(nxscli_np.plugin.configs_list, list)
+    assert isinstance(nxscli_np.ext_plugins.plugins_list, list)
+    assert isinstance(nxscli_np.ext_commands.commands_list, list)
